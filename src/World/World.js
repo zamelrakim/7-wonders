de1776bd98fd6356301260541713464f10dfab19
Zamel Hill <zrhill@gmail.com>
2020-04-29 19:00:32 -0400

    Updated World Component

--- src/World/World.js
+++ src/World/World.js
@@ -1,41 +1,42 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { WorldMap } from 'grommet'
 
 export default function World(props) {
   console.log(props)
+  let history = useHistory()
 
   const continents = [
     {
       name: "Africa",
       color: "graph-1",
-      onClick: (e) => { props.setContinent(e); }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
       // onHover: (e) => { console.log(e)}
     },
     {
       name: "Asia",
       color: "graph-1",
-      onClick: (e) => { console.log(e) }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
     },
     {
       name: 'Australia',
       color: 'graph-1',
-      onClick: (e) => { console.log(e) }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
     },
     {
       name: 'Europe',
       color: 'graph-1',
-      onClick: (e) => { console.log(e) }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
     },
     {
       name: 'North America',
       color: 'graph-1',
-      onClick: (e) => { console.log(e) }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
     },
     {
       name: 'South America',
       color: 'graph-1',
-      onClick: (e) => { console.log(e) }
+      onClick: (e) => { props.setContinent(e); history.push(`/${e}/`) }
     }
   ]
   return (
